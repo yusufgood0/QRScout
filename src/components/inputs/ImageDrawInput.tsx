ffe24dbb9321f152f988f31cd072bef95893d764
Yusuf amin <yusufgood0@gmail.com>
2026-04-14 02:10:00 -0400

    made circle end 5x bigger

--- src/components/inputs/ImageDrawInput.tsx
+++ src/components/inputs/ImageDrawInput.tsx
@@ -56,7 +56,7 @@ function DrawPolyLineStaticColor(points: VertexPoint[], ctx: CanvasRenderingCont
 
     ctx.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
     ctx.beginPath();
-    ctx.arc(last.x, last.y, 5, 0, Math.PI * 2); // radius = 5
+    ctx.arc(last.x, last.y, 25, 0, Math.PI * 2); // radius = 5
     ctx.fill();
   }
   
